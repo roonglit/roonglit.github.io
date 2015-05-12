73bac612832e11b12fc5d53d0546279fc9f64ab6
Roonglit Chareonsupkul <roonglit@gmail.com>
2015-05-12 23:48:07 +0700

    add particles

--- src/_assets/javascripts/app.js
+++ src/_assets/javascripts/app.js
@@ -2,4 +2,5 @@
 //= require bootstrap/dist/js/bootstrap
 //= require plugins_merged
 //= require main
-
+//= require jquery.particleground.min
+//= require particle-init
---
+++ src/_assets/javascripts/particle-init.js
@@ -0,0 +1,16 @@
+(function(){
+  "use strict";
+
+  $(document).ready(function() {
+    $('.particles').particleground({
+      dotColor: 'rgba(120,233,253,0.2)',
+      lineColor: 'rgba(120,233,253,0.2)',
+      density:18000,
+      proximity:80,
+      directionY:'right',
+      directionX:'right',
+      maxSpeedX:0.4
+    });
+
+  });
+})();
